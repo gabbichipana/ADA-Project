e5b06ea0820b1b0f95ebd98eebeda8695e738ac7
gabbichipana <42560116+gabbichipana@users.noreply.github.com>
2021-06-16 19:47:31 -0500

    Update earleyParser.h

--- earleyParser.h
+++ earleyParser.h
@@ -59,7 +59,20 @@ void EarleyParser::SCANNER(State s)
         chart.getContent()[j+1].push_back(temp);
     }
 }
-
+void EarleyParser::SCANNER(State s)
+{
+    State temp;	
+	int j = s.getIdx2();
+	int iterator = 0;
+	for(State i : chart[s.j])
+		if(i.[pointIdx] != al final) //el punto no esta al final 
+		{ 
+			temp.setIdx1(iterator);
+			temp.setIdx2(j);
+			chart.getContent()[j].push_back(temp);
+		}
+		iterator++;
+}
 void EarleyParser::setGrammar(std::string fileName)
 {
     this->grammar.readGrammarFromTXT(fileName);
